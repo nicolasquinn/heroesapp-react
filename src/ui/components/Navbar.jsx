c5a6f50ad4618a722be70d8895a9fc2ecc2070cb
Nicolas Quinn <nclsquinn@gmail.com>
2022-10-16 11:57:18 -0300

    Class 191 - Navigate push/replace: useNavigate

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,15 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true,
+        })
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -37,7 +46,9 @@ export const Navbar = () => {
                         Nicolas
                     </span>
 
-                    <button className="nav-item nav-link btn">
+                    <button 
+                    className="nav-item nav-link btn"
+                    onClick={ onLogout }>
                         Logout
                     </button>
 
