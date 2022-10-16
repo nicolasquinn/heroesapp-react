304ec150a5b71d44356022873d141f6ea9b84a79
Nicolas Quinn <nclsquinn@gmail.com>
2022-10-16 15:14:34 -0300

    Class 196 - HeroPage component style

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,9 +1,10 @@
-import { Navigate, useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
 
 
 export const HeroPage = () => {
 
+  const navigate = useNavigate();
   const { id } = useParams();
   const hero = getHeroById(id);
 
@@ -11,7 +12,41 @@ export const HeroPage = () => {
     return <Navigate to="/marvel"/>;
   }
 
+  const onNavigateBack = () => {
+    id.includes('marvel') ? navigate('/marvel') : navigate('/dc');
+  }
+
   return (
-    <h1>{ hero.superhero }</h1>
+    <div className="row mt-5">
+
+      <div className="col-4">
+        <img 
+        src={ `/assets/heroes/${id}.jpg` } 
+        alt={ hero.superhero } 
+        className="img-thumbnail" 
+        />
+      </div>
+
+      <div className="col-8">
+        <h3 className="fw-bold">{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"> <b>Publisher:</b> { hero.publisher }</li>
+          <li className="list-group-item"> <b>Alter ego:</b> { hero.alterEgo }</li>
+          <li className="list-group-item"> <b>First appearance</b> { hero.firstAppearance }</li>
+        </ul>
+
+        <h5 className="mt-3 fw-bold">Characters</h5>
+        <p>{ hero.characters }</p>
+
+        <button 
+        className="btn btn-outline-primary"
+        onClick={ onNavigateBack }>
+          Back
+        </button>
+
+      </div>    
+
+    </div>
+
   )
 }
