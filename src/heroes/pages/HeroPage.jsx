a18cc21a1c965cf5982c5f4f9d60f12aae275738
Nicolas Quinn <nclsquinn@gmail.com>
2022-10-16 15:19:36 -0300

    Class 197 - Applying useMemo

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,12 +1,13 @@
+import { useMemo } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
 
 
 export const HeroPage = () => {
 
-  const navigate = useNavigate();
   const { id } = useParams();
-  const hero = getHeroById(id);
+  const navigate = useNavigate();
+  const hero = useMemo( () => getHeroById(id), [id] );
 
   if ( !hero ) {
     return <Navigate to="/marvel"/>;
