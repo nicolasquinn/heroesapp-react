6bd7dc73d185e4c2b33fca5d105c3fa0f38acbdf
Nicolas Quinn <nclsquinn@gmail.com>
2022-10-17 16:13:40 -0300

    Few CSS changes

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -17,29 +17,30 @@ export const HeroCard = ({
         <div className="card rounded-4">
 
             <div className="row no-gutter">
-                <div className="col-5" >
-                    <img src={ heroImgUrl } alt={ superhero } className="card-img rounded-4 p-1"/>
-                </div>
-            </div>
 
-            <div className="col-7">
-
-                <div className="card-body">
-
-                    <h5 className="card-title fw-bold">{ superhero }</h5>
-                    <p className="card-text">{ alterEgo }</p>
-                    { ( characters !== alterEgo ) && <p className="fw-" >{ characters }</p> } 
-
-                    <p className="card-text">
-                        <small className="text-muted">{ firstAppearance }</small>
-                    </p>
-
-                    <Link to={`/hero/${id}`}>
-                        More
-                    </Link>
-
+                <div className="col-6">
+                    <img src={ heroImgUrl } alt={ superhero } className="card-img rounded-4"/>
                 </div>
 
+                <div className="col-6">
+
+                    <div className="card-body p-0 pt-3">
+
+                        <h5 className="card-title fw-bold">{ superhero }</h5>
+                        <p className="card-text">{ alterEgo }</p>
+                        { ( characters !== alterEgo ) && <p>{ characters }</p> } 
+
+                        <p className="card-text">
+                            <small className="text-muted">{ firstAppearance }</small>
+                        </p>
+
+                        <Link to={`/hero/${id}`}>
+                            More
+                        </Link>
+
+                    </div>
+
+                </div>
             </div>
 
         </div>
