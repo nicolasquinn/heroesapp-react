06ac534a64c2e84630762e60ec576a816e55478b
Nicolas Quinn <nclsquinn@gmail.com>
2022-10-18 12:27:07 -0300

    Class 210 - Keep the user active

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,25 +3,35 @@ import { AuthContext } from "./AuthContext"
 import { authReducer } from "./authReducer"
 import { types } from "../types/types"
 
-const initialState = {
-    logged: false,
+const init = () => {
+
+  const user = JSON.parse(localStorage.getItem('user'));
+  return {
+    logged: !!user,
+    user,
+  }
+
 }
 
 export const AuthProvider = ({ children }) => {
 
-    const [ authState, dispatch ] = useReducer( authReducer, initialState );
+    const [ authState, dispatch ] = useReducer( authReducer, {}, init );
 
     const login = ( name = '' ) => {
 
+      const user = {
+        id: Date.now(),
+        name,
+      }
+
       const action = {
         type: types.login,
-        payload: {
-          id: Date.now(),
-          name,
-        },
+        payload: user,
       }
 
       dispatch(action);
+
+      localStorage.setItem('user', JSON.stringify(user));
 
     }
 
