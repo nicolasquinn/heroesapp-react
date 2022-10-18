6db0bb2cd21f125f033fe7552e1e0b785aa43d00
Nicolas Quinn <nclsquinn@gmail.com>
2022-10-18 13:09:04 -0300

    Class 212 - Private routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
   return (
@@ -10,7 +11,11 @@ export const AppRouter = () => {
 
         <Route path="login" element={ <LoginPage/> } />
 
-        <Route path="/*" element={ <HeroesRoutes /> } />
+        <Route path="/*" element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        } />
 
       </Routes>
       
